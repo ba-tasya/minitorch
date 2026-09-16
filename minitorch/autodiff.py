44ea91c849e07e70f49f8934140eae3a9f7bab79
Anastasiia <ba.tasya@yandex.ru>
2026-09-16 21:50:02 +0300

    MiniTorch

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -22,8 +22,11 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError('Need to implement for Task 1.1')
+    vals_pos = list(vals)
+    vals_neg = list(vals)
+    vals_pos[arg] += epsilon
+    vals_neg[arg] -= epsilon
+    return (f(*vals_pos) - f(*vals_neg)) / (2 * epsilon)
 
 
 variable_count = 1
@@ -61,8 +64,19 @@ def topological_sort(variable: Variable) -> Iterable[Variable]:
     Returns:
         Non-constant Variables in topological order starting from the right.
     """
-    # TODO: Implement for Task 1.4.
-    raise NotImplementedError('Need to implement for Task 1.4')
+    visited = []
+    order = []
+    def dfs(var: Variable) -> None:
+        if var.unique_id in visited:
+            return
+        visited.append(var.unique_id)
+        for parent in var.parents:
+            dfs(parent)
+        order.append(var)
+    dfs(variable)
+    order.reverse()
+
+    return order
 
 
 def backpropagate(variable: Variable, deriv: Any) -> None:
@@ -76,8 +90,20 @@ def backpropagate(variable: Variable, deriv: Any) -> None:
 
     No return. Should write to its results to the derivative values of each leaf through `accumulate_derivative`.
     """
-    # TODO: Implement for Task 1.4.
-    raise NotImplementedError('Need to implement for Task 1.4')
+    order = topological_sort(variable)
+    derivatives = {variable.unique_id: deriv}
+
+    for var in order:
+        d_output = derivatives[var.unique_id]
+        if var.is_leaf():
+            var.accumulate_derivative(d_output)
+        else:
+            for parent, d_parent in var.chain_rule(d_output):
+                if parent.is_constant():
+                    continue
+                derivatives[parent.unique_id] = (
+                    derivatives.get(parent.unique_id, 0.0) + d_parent
+                )
 
 
 @dataclass
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -107,16 +107,18 @@ def test_sigmoid(a: float) -> None:
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert sigmoid(a) >= 0.0
+    assert sigmoid(a) <= 1.0
+    assert sigmoid(-a) == 1.0 - sigmoid(a)
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     "Test the transitive property of less-than (a < b and b < c implies a < c)"
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert lt(a, b) == 1.0
+    assert lt(b, c) == 1.0
+    assert lt(a, c) == 1.0
 
 
 @pytest.mark.task0_2
@@ -125,8 +127,10 @@ def test_symmetric() -> None:
     Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert mul(1.0, 2.0) == mul(2.0, 1.0)
+    assert mul(2.0, 3.0) == mul(3.0, 2.0)
+    assert mul(3.0, 5.0) == mul(5.0, 3.0)
+    assert mul(-3.0, 12.0) == mul(12.0, -3.0)
 
 
 @pytest.mark.task0_2
@@ -135,8 +139,7 @@ def test_distribute() -> None:
     Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert mul(2.0, add(3.0, 4.0)) == add(mul(2.0, 3.0), mul(2.0, 4.0))
 
 
 @pytest.mark.task0_2
@@ -144,8 +147,9 @@ def test_other() -> None:
     """
     Write a test that ensures some other property holds for your functions.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert sigmoid(add(1.0, 2.0)) == sigmoid(1.0) + sigmoid(2.0)
+    assert relu(add(1.0, 2.0)) == relu(1.0) + relu(2.0)
+
 
 
 # ## Task 0.3  - Higher-order functions
@@ -173,8 +177,7 @@ def test_sum_distribute(ls1: List[float], ls2: List[float]) -> None:
     Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
     is the same as the sum of each element of `ls1` plus each element of `ls2`.
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+    assert_close(sum(ls1) + sum(ls2), sum(addLists(ls1, ls2)))
 
 
 @pytest.mark.task0_3
